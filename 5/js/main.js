361fd9cbd80bf26de91280a74c04dd02cc5ae8f8
Keks <keks@htmlacademy.ru>
2022-06-23 15:46:43 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,6 @@
+import { createAccomodationCards } from './cards.js';
+
+const ACCOMODATION_COUNT = 10;
+
+createAccomodationCards(ACCOMODATION_COUNT);
+
---
+++ 5/js/mocks/data.js
@@ -0,0 +1,95 @@
+import { getRandomInteger, getRandomFloat, getRandomArrayElement, getRandomElements, generateAccomodationNumber } from './util.js';
+
+const TITLES = [
+  'Комфортабельное проживание в красивом районе',
+  'Удобное проживание рядом с транспортными развязками',
+  'Превосходный вариант для семейных пар',
+  'Удобный вариант для активных путешественников',
+  'Удачный вариант для любителей культуры и искусства',
+  'Превосходное размещение, домашние питомцы приветствуются',
+];
+
+const DESCRIPTIONS = [
+  'Замечательное расположение - тихий район, тенистый сквер и детская площадка.',
+  'Предложение подходит активным молодым людям, любящим громкую музыку и прогулки по ночам.',
+  'Удобная транспортная развязка, легко добраться в любую точку города.',
+  'В пешей доступности музей, кинотеатр и супермаркет.',
+  'Наш девиз -  комфорт, чистота и удобство.',
+  'К сожалению, мы не размещаем гостей с домашними питомцами.',
+  'В пешей доступности бассейн и превосходный спортивный комплекс',
+];
+
+const ACCOMODATION_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURS = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+/**
+ *Возвращает объект accomodation.
+ * @returns {Object} - искомый объект
+ */
+const getAccomodation = () => {
+  const accomodationNumber = generateAccomodationNumber();
+  const randomLat = getRandomFloat(35.65000, 35.70000, 5);
+  const randomLng = getRandomFloat(139.70000, 139.80000, 5);
+  return {
+    author: {
+      avatar: `img/avatars/user${(accomodationNumber < 10) ? `0${accomodationNumber}` : accomodationNumber}.png`,
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${randomLat} ${randomLng}`,
+      price: getRandomInteger(100, 10000),
+      type: getRandomArrayElement(ACCOMODATION_TYPES),
+      rooms: getRandomInteger(1, 10),
+      guests: getRandomInteger(1, 10),
+      checkin: getRandomArrayElement(CHECKINS),
+      checkout: getRandomArrayElement(CHECKOUTS),
+      features: getRandomElements(FEATURS),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomElements(PHOTOS),
+    },
+    location: {
+      lat: randomLat,
+      lng: randomLng,
+    },
+  };
+};
+
+/**
+ *Возвращает массив объектов заданной длины. Объекты создаются с помощью  callback-функции getAccomodation.
+ */
+const createAccomodations = (accomodationAmount) => Array.from({ length: accomodationAmount }, getAccomodation);
+
+export { createAccomodations };
+
